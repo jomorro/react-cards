c45ac6af5432da519c160f6b995c815733203cd5
jomorro <morrowjoe0@gmail.com>
2019-02-26 09:39:02 -0500

    Updates

---
+++ precache-manifest.8406c0a8c87d9d0062a49189351eb851.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "55b8bd85985d209c993a",
+    "url": "/react-cards/static/css/main.48ec18b1.chunk.css"
+  },
+  {
+    "revision": "55b8bd85985d209c993a",
+    "url": "/react-cards/static/js/main.55b8bd85.chunk.js"
+  },
+  {
+    "revision": "e972a97fedc948707364",
+    "url": "/react-cards/static/js/1.e972a97f.chunk.js"
+  },
+  {
+    "revision": "fe733ad86d5f0fd1cd70",
+    "url": "/react-cards/static/js/runtime~main.fe733ad8.js"
+  },
+  {
+    "revision": "c5a8e985ec09bd9b9c4a99fe97b6f375",
+    "url": "/react-cards/index.html"
+  }
+];
